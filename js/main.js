62ad1a1c0dd2eb65a2bf79f381ad99bc13d3e36a
Md.Mahfuzar Rahman <58438084+mahfuzar07@users.noreply.github.com>
2021-07-26 04:21:47 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,22 @@ var cleave = new Cleave('.card-no', {
   creditCard: true,
   delimiter: '-',
   blocks: [4, 4, 4, 4],
-  uppercase: true,
-  onCreditCardTypeChanged: function (type) {},
+
+  onCreditCardTypeChanged: function (type) {
+    type === 'visa'
+      ? document.querySelector('.fa-cc-visa').classList.add('active')
+      : document.querySelector('.fa-cc-visa').classList.remove('active');
+
+    type === 'mastercard'
+      ? document.querySelector('.fa-cc-mastercard').classList.add('active')
+      : document.querySelector('.fa-cc-mastercard').classList.remove('active');
+
+    type === 'jcb'
+      ? document.querySelector('.fa-cc-jcb').classList.add('active')
+      : document.querySelector('.fa-cc-jcb').classList.remove('active');
+
+    type === 'amex'
+      ? document.querySelector('.fa-cc-amex').classList.add('active')
+      : document.querySelector('.fa-cc-amex').classList.remove('active');
+  },
 });
